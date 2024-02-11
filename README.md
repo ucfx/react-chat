43e7cd973e2c7a1df9b022002db5fc797e0652c9
ucfx <65844371+ucfx@users.noreply.github.com>
2024-02-12 00:55:57 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1 align="center"> Real Time Chat </h1>
---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      hooks: "/src/hooks",
+      styles: "/src/styles",
+      assets: "/src/assets",
+      data: "/src/data",
+    },
+  },
+});
